9d8b40cfc36042567bdf4a5c1a3118b9a3288960
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-12 23:41:15 +0100

    re-create an instance with the dictionary representation that we created previously with to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -9,15 +9,23 @@ A module that representes a BaseModel class
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     A class BaseModel that defines all common
     attributes/methods for other classes
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -30,6 +38,3 @@ class BaseModel:
         self.updated_at = self.updated_at.isoformat()
         self.__dict__.update({'__class__': self.__class__.__name__})
         return self.__dict__
-
-
-
