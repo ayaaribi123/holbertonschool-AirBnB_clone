aeb5c00d4af1df493d4123a6f4078f8e8f5036ec
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-13 02:20:20 +0100

    link the BaseModel to FileStorage by using the variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ A module that representes a BaseModel class
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -26,12 +27,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save
 
     def to_dict(self):
         self.created_at = self.created_at.isoformat()
