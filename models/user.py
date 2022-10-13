5eb49359c3391aebf25e1efaa9d2690735b6e5cc
ayaaribi123 <ayaaribi11@gmail.com>
2022-10-13 22:40:55 +0100

    user

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+
+class
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+	"""class user"""
+	email = ""
+	password = ""
+	first_name = ""
+	last_name =	""
