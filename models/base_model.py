a9b231015c9efd47ebebad1ac954409a8bcbb199
ayaaribi123 <ayaaribi11@gmail.com>
2022-10-13 16:45:37 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import models
 from uuid import uuid4
 
+
 class BaseModel:
     """
     A class BaseModel that defines all common
@@ -42,7 +43,8 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        self.__dict__.update({'__class__': self.__class__.__name__})
-        return self.__dict__
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
