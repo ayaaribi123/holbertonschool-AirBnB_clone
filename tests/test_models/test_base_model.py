3b5d51cca7f74616b38686804b016e36f690fd91
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-15 16:15:40 +0100

    test file for BaseModel class

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class Test_Class_BaseModel(unittest.TestCase):
+    def test_class_type(self):
+        A = BaseModel()
+
+        self.assertTrue(A, '<class base_model.BaseModel>')
+
+    def test_is_instance(self):
+        B = BaseModel()
+
+        self.assertIsInstance(B, BaseModel)
+
+    def test_adding_attributes(self):
+        a = BaseModel()
+        a.name = "mYmODEL"
+        a.number = 90
+
+        self.assertTrue(hasattr(a, 'name'))
+        self.assertEqual(a.name, 'mYmODEL')
+        self.assertEqual(a.number, 90)
+
+    def test_attributes(self):
+        C = BaseModel()
+
+        self.assertTrue(type(C.id), '<class str>')
+        self.assertTrue(type(C.created_at), '<class datetime.datetime>')
+        self.assertTrue(type(C.updated_at), '<class datetime.datetime>')
+
+    def testFunction_save(self):
+        b = BaseModel()
+        b.save()
+        now_time = datetime.now()
+
+        self.assertEqual(b.updated_at.replace(microsecond=0), now_time.replace(microsecond=0))
+
+    def testFunction_to_dict(self):
+        c = BaseModel()
+        c_json = c.to_dict()
+
+        self.assertIn('__class__', c_json)
+        self.assertTrue(type(c.created_at), '<class str>')
+        self.assertTrue(type(c.updated_at), '<class str>')
