d712159055a27e75abf548dde352278e2a7012f2
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-15 18:29:51 +0100

    test file for FileStorage class

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import unittest
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class Test_Class_FileStorage(unittest.TestCase):
+    def test_class_attributes(self):
+
+    # To be continued later
