6b29e6be72a3d8056aff5e20818ca92672ca1cfa
ayaaribi123 <ayaaribi11@gmail.com>
2022-10-19 17:51:23 +0100

    user

--- models/user.py
+++ models/user.py
@@ -9,8 +9,8 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-	"""class User that is a subclass of BaseModel class"""
-	email = ""
-	password = ""
-	first_name = ""
-	last_name = ""
+    """class User that is a subclass of BaseModel class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
