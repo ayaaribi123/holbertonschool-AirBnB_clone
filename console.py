5f2db188497b234a7fd98db134af5920d3c81f7d
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-10-19 13:14:21 +0100

    fix code for all command

--- console.py
+++ console.py
@@ -83,7 +83,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         args = arg.split()
-        if len(args) > 0 and args[0] != "BaseModel":
+        if ((len(args)) > 0 and (args[0] != "BaseModel")):
             print("** class doesn't exist **")
         storage.reload()
         objects = storage.all()
